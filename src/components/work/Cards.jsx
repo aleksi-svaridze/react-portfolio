75bfd847bdfe4d3584c7a34fbd6a63ecba130bfa
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-06-28 11:19:55 +0400

    Finish: Work component

--- src/components/work/Cards.jsx
+++ src/components/work/Cards.jsx
@@ -1,27 +1,29 @@
 const Card = ({content}) => {
 
+    let {id, imgSrc, desc} = content;
+
     return (
         <div 
             style={{
-                width: `${content.id === 2 ? '440px' : '380px'}`, 
-                height: `${content.id === 2 ? '330px' : '280px'}`, 
-                zIndex: `${content.id === 2 ? '10' : '5'}`,
+                width: `${id === 2 ? '440px' : '380px'}`, 
+                height: `${id === 2 ? '330px' : '280px'}`, 
+                zIndex: `${id === 2 ? '10' : '5'}`,
                 backdropFilter: 'blur(10px)', 
-                marginLeft: `${content.id === 3 && '-30px'}`, 
-                marginRight: `${content.id === 1 && '-30px'}`}} 
-            key={content.id}
+                marginLeft: `${id === 3 && '-30px'}`, 
+                marginRight: `${id === 1 && '-30px'}`}} 
+            key={id}
             className="overflow-hidden position-relative py-4 pt-2 rounded-5 bg-white-40">
             <div 
                 style={{
-                    backgroundImage: `url(${content.imgSrc})`,
-                    backgroundSize: `100% ${content.id === 2 ? '90%' : '100%'}`,
+                    backgroundImage: `url(${imgSrc})`,
+                    backgroundSize: `100% ${id === 2 ? '90%' : '100%'}`,
                     backgroundRepeat: 'no-repeat',
-                    backgroundPosition: `${content.id === 2 ? '0% 0%' : 'center'}`,
+                    backgroundPosition: `${id === 2 ? '0% 0%' : 'center'}`,
                 }} 
                 className="w-100 h-100"></div>
 
             <a href="/" className="text-decoration-none rounded-bottom-5 py-3 mb-0 text-white text-center position-absolute start-0 bottom-0 end-0 fw-bold fs-4 bg-black-30">
-                {content.desc}
+                {desc}
             </a>
         </div>
     )
--- src/components/work/Desktop.jsx
+++ src/components/work/Desktop.jsx
@@ -1,8 +1,6 @@
 import WorkBgMedium from '../../media/images/work/bg-medium.svg';
 import WorkBgBig from '../../media/images/work/bg-big.svg';
 
-import Card from './Cards';
-
 import { useWindowSize } from 'usehooks-ts';
 
 import CardImageDesigns from '../../media/images/work/designs.svg';
@@ -24,7 +22,32 @@ const Desktop = () => {
             
             <div className="container position-absolute top-50 start-50 translate-middle">
                 <div className="row justify-content-center align-items-center">
-                    { cardContent.map(content => <Card content={content} />) }
+
+                { cardContent.map(content => ( 
+                    <div 
+                        style={{
+                            width: `${content.id === 2 ? '440px' : '380px'}`, 
+                            height: `${content.id === 2 ? '330px' : '280px'}`, 
+                            zIndex: `${content.id === 2 ? '10' : '5'}`,
+                            backdropFilter: 'blur(10px)', 
+                            marginLeft: `${content.id === 3 && '-30px'}`, 
+                            marginRight: `${content.id === 1 && '-30px'}`}} 
+                        key={content.id}
+                        className="overflow-hidden position-relative py-4 pt-2 rounded-5 bg-white-40">
+                        <div 
+                            style={{
+                                backgroundImage: `url(${content.imgSrc})`,
+                                backgroundSize: `100% ${content.id === 2 ? '90%' : '100%'}`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundPosition: `${content.id === 2 ? '0% 0%' : 'center'}`,
+                            }} 
+                            className="w-100 h-100"></div>
+
+                        <a href="/" className="text-decoration-none rounded-bottom-5 py-3 mb-0 text-white text-center position-absolute start-0 bottom-0 end-0 fw-bold fs-4 bg-black-30">
+                            {content.desc}
+                        </a>
+                    </div>
+                    ) )} 
                 </div>
             </div>
         </div>
