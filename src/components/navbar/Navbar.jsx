3f547c6810595ec48953ce46eeb8df41cce60496
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-10-30 11:42:50 +0400

    Hide navbar links before sm breakpoint

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
     return (
         <div className="position-absolute top-0 start-0 end-0 z-3" style={{paddingTop: width >= 768 ? '40px' : '20px'}}>
             <nav style={{columnGap: size + 'px'}} className='d-flex align-items-center justify-content-center'>
-                <a href="/#home" className='fw-medium text-decoration-none' style={{fontSize: width >= 768 ? '17px' : '14px'}}>Home</a>
-                <a href="/#about" className='fw-medium text-decoration-none' style={{fontSize: width >= 768 ? '17px' : '14px'}}>About me</a>
+                <a href="/#home" className='fw-medium text-decoration-none d-none d-sm-block' style={{fontSize: width >= 768 ? '17px' : '14px'}}>Home</a>
+                <a href="/#about" className='fw-medium text-decoration-none d-none d-sm-block' style={{fontSize: width >= 768 ? '17px' : '14px'}}>About me</a>
                 <img src={Logo} style={{width: width >= 768 ? '46px' : '32px'}} alt="" />
-                <a href="/#work" className='fw-medium text-decoration-none' style={{fontSize: width >= 768 ? '17px' : '14px'}}>Work</a>
-                <a href="/#contact" className='fw-medium text-decoration-none' style={{fontSize: width >= 768 ? '17px' : '14px'}}>Contact</a>
+                <a href="/#work" className='fw-medium text-decoration-none d-none d-sm-block' style={{fontSize: width >= 768 ? '17px' : '14px'}}>Work</a>
+                <a href="/#contact" className='fw-medium text-decoration-none d-none d-sm-block' style={{fontSize: width >= 768 ? '17px' : '14px'}}>Contact</a>
             </nav>
         </div>
     )
